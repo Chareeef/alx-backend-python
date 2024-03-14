2fd337f5c168d1b36f808e5106c2cd3bde8fad4b
Chareeef <youssef.charif.h@gmail.com>
2024-03-14 16:04:52 +0000

    Change sum_mixed_list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """ Type-annotated `sum_mixed_list` function """
-from functools import reduce
 from typing import List, Union
 
 
@@ -8,4 +7,9 @@ def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """ Takes a list mxd_lst of floats OR integers as argument
     and returns their sum as a float
     """
-    return reduce(lambda x, y: x + y, mxd_lst, 0)
+    result: float = 0
+
+    for n in mxd_lst:
+        result += n
+
+    return result
