1d9b975925d8fd4132fb1962d932a1420f447aa2
Chareeef <youssef.charif.h@gmail.com>
2024-03-14 17:10:02 +0000

    Implement type-annotated make_multiplier function

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,13 +4,7 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
-    """
-    Takes a list mxd_lst of floats OR integers as argument
+    """ Takes a list mxd_lst of floats OR integers as argument
     and returns their sum as a float
     """
-    result: float = 0
-
-    for n in mxd_lst:
-        result += n
-
-    return float(result)
+    return float(sum(mxd_lst))
