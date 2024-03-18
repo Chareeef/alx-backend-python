ae152484b2b5c6da0ec075bcd77328aff6810417
Chareeef <youssef.charif.h@gmail.com>
2024-03-18 15:51:27 +0000

    Use asyncio.as_completed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Execute multiple coroutines concurrently"""
+import asyncio
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
@@ -8,8 +10,13 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     """Spawn wait_random `n` times with the specified `max_delay`
     wait_n should return the list of all the delays (float values)
     """
-    delays = []
+    futures = []
     for _ in range(n):
-        delays.append(await wait_random(max_delay))
+        futures.append(asyncio.create_task(wait_random(max_delay)))
+
+    delays = []
+    for future in asyncio.as_completed(futures):
+        delay = await future
+        delays.append(delay)
 
     return delays
