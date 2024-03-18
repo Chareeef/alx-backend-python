0c2c291e69872b979645aace1a7e25d750d2337f
Chareeef <youssef.charif.h@gmail.com>
2024-03-18 15:55:28 +0000

    Use typing.List

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Execute multiple coroutines concurrently"""
 import asyncio
+import typing
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> typing.List[float]:
     """Spawn wait_random `n` times with the specified `max_delay`
     wait_n should return the list of all the delays (float values)
     """
