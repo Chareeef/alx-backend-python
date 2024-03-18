f3c360880d855a8754b17bb541be3d9e77a6f19e
Chareeef <youssef.charif.h@gmail.com>
2024-03-18 17:02:49 +0000

    Implement task_wait_n that spawns and runs task_wait_random

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-"""Execute multiple coroutines concurrently"""
+"""Execute multiple coroutines (tasks)concurrently"""
 import asyncio
 import typing
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> typing.List[float]:
-    """Spawn wait_random `n` times with the specified `max_delay`
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+    """Spawn and run task_wait_random `n` times with the specified `max_delay`
     wait_n should return the list of all the delays (float values)
     """
     futures = []
     for _ in range(n):
-        futures.append(asyncio.create_task(wait_random(max_delay)))
+        futures.append(task_wait_random(max_delay))
 
     delays = []
     
