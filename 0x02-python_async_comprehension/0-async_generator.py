11d32455350f69e1338a193910915c194e94363f
Chareeef <youssef.charif.h@gmail.com>
2024-03-19 13:57:52 +0000

    Correct async_generator type annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""A Basic Asynchronous generator"""
+"""A Basic Asynchronous Generator"""
 import asyncio
 import random
-from typing import AsyncIterator
+from typing import Generator
 
 
-async def async_generator() -> AsyncIterator[float]:
+async def async_generator() -> Generator[float, None, None]:
     """10 times sleep and yield a random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
