e5cbf3539307f5f85ceac6bde1ff847a1555035c
Chareeef <youssef.charif.h@gmail.com>
2024-03-19 12:29:55 +0000

    Implement a basic async comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """A Basic Asynchronous generator"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Iterator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Iterator[float]:
     """10 times sleep and yield a random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
