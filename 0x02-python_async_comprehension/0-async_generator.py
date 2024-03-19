8909519cfeccf077100bff082af5f7b4fb155e55
Chareeef <youssef.charif.h@gmail.com>
2024-03-19 13:05:33 +0000

    Measure the runtime of async_comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """A Basic Asynchronous generator"""
 import asyncio
 import random
-from typing import Iterator
+from typing import AsyncIterator
 
 
-async def async_generator() -> Iterator[float]:
+async def async_generator() -> AsyncIterator[float]:
     """10 times sleep and yield a random number between 0 and 10"""
     for _ in range(10):
         await asyncio.sleep(1)
