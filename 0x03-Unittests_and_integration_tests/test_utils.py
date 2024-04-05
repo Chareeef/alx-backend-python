b45463239c56045b7de16c3654e05a277608858f
Chareeef <youssef.charif.h@gmail.com>
2024-04-05 11:30:47 +0000

    Test access_nested_map KeyError exception with parameterized.expand

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
          """Test utils.access_nested_map results"""
          self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "'a'"),
+        ({"a": 1}, ("a", "b"), "'b'")
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, error_msg):
+        """Test utils.access_nested_map KeyError exception"""
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(error.exception), error_msg)
